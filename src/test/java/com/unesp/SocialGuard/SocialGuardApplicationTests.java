9349fac24e4c08f4520657161080ab88fe1b148c
RicardoMatsumura <fuzilo@hotmail.com>
2026-09-22 20:03:53 -0300

    Commit inicial

---
+++ src/test/java/com/unesp/SocialGuard/SocialGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unesp.SocialGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
